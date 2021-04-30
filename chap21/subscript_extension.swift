98e2fb1c4952b15d041282b1059bef6d97a5f7a7
kim-hyunjin <hyunjin1612@gmail.com>
2021-04-30 14:25:25 +0900

    extension

---
+++ chap21/subscript_extension.swift
@@ -0,0 +1,18 @@
+extension String {
+    subscript(appendValue: String) -> String {
+        return self + appendValue
+    }
+
+    subscript(repeatCount: UInt) -> String {
+        var str: String = ""
+
+        for _ in 0..<repeatCount {
+            self += self
+        }
+
+        return str
+    }
+}
+
+print("abc"["def"])
+print("abc"[3])

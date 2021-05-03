885cf56f7296efd958b8ca7c88e91d40b9b08e68
Kim <hyunjin1612@gmail.com>
2021-05-03 23:54:03 +0900

    try!, rethrows, defer

--- chap28_error_handling/optional_try.swift
+++ chap28_error_handling/optional_try.swift
@@ -19,3 +19,9 @@ print(x as Any) // nil
 let y: Optional = try? someThrowingFunction(shouldThrowError: false)
 print(y as Any) // Optional(100)
 // someThrowingFunction(shouldThrowError:)의 반환타입이 Int라도 try? 표현을 사용하면 옵셔널이 된다.
+
+// 오류가 발생하지 않을 것이라고 확신하는 방법 - try!
+// => 실제 오류가 발생하면 런타임 오류 발생, 프로그램이 강제 종료된다.
+var z: Int = try! someThrowingFunction(shouldThrowError: false)
+print(z)
+// z = try! someThrowingFunction(shouldThrowError: true) // 런타임 오류!
